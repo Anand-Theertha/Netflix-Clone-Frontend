cad276c15557525378a660fed93e1879e2c44551
AnandTheertha <anandtheerthav3@gmail.com>
2023-04-25 17:38:06 +0530

    Netflix Register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 
-import Home from './pages/home/Home';
-import Watch from './pages/watch/Watch';
+// import Home from './pages/home/Home';
+// import Watch from './pages/watch/Watch';
+
+import Register from './pages/register/Register';
+
 
 function App() {
   return (
-    <Watch />
+    <Register />
   );
 } 
 
